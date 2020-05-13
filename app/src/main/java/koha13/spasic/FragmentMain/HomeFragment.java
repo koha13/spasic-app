27ad4fa77a0bafde407836a6d89f3c35e0903c73
khoa <policekhoa13101998@gmail.com>
2020-05-13 19:30:39 +0700

    Add pull to refresh to main and rank fragment

--- app/src/main/java/koha13/spasic/FragmentMain/HomeFragment.java
+++ app/src/main/java/koha13/spasic/FragmentMain/HomeFragment.java
@@ -4,10 +4,12 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -16,12 +18,13 @@ import koha13.spasic.R;
 import koha13.spasic.adapter.BigCVAdapter;
 import koha13.spasic.model.Song;
 
-public class HomeFragment extends Fragment {
+public class HomeFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
     public HomeFragment() {
         // Required empty public constructor
     }
 
     private RecyclerView.LayoutManager layoutManager;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -32,6 +35,8 @@ public class HomeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_apple, container, false);
+
+        //Recycler view big Cardview
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerViewSongMain);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity());
@@ -42,6 +47,33 @@ public class HomeFragment extends Fragment {
         songs.add(new Song("Test3","Artist",123));
         BigCVAdapter songCardAdapter = new BigCVAdapter(songs, getActivity());
         recyclerView.setAdapter(songCardAdapter);
+
+        //Swipe refresh
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.main_swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        //First load when view created
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                loadRecyclerViewData();
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onRefresh() {
+        loadRecyclerViewData();
+    }
+
+    private void loadRecyclerViewData(){
+        // Showing refresh animation before making http call
+        mSwipeRefreshLayout.setRefreshing(true);
+        Toast.makeText(getActivity(), "Ahlo", Toast.LENGTH_SHORT).show();
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 }
--- app/src/main/java/koha13/spasic/FragmentMain/RankFragment.java
+++ app/src/main/java/koha13/spasic/FragmentMain/RankFragment.java
@@ -6,10 +6,12 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -18,7 +20,7 @@ import koha13.spasic.R;
 import koha13.spasic.adapter.SongCardAdapter;
 import koha13.spasic.model.Song;
 
-public class RankFragment extends Fragment {
+public class RankFragment extends Fragment implements SwipeRefreshLayout.OnRefreshListener{
     public RankFragment() {
         // Required empty public constructor
     }
@@ -31,12 +33,14 @@ public class RankFragment extends Fragment {
 
     private RecyclerView.LayoutManager layoutManager;
     private Spinner spinner;
-
+    private SwipeRefreshLayout mSwipeRefreshLayout;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_orange, container, false);
+
+        //Add spinner
         spinner = (Spinner) rootView.findViewById(R.id.regionSpinner);
         List<String> list = new ArrayList<>();
         list.add("Nhạc US/UK");
@@ -46,6 +50,8 @@ public class RankFragment extends Fragment {
         ArrayAdapter<String> adapter = new ArrayAdapter(this.getActivity(), android.R.layout.simple_spinner_item,list);
         adapter.setDropDownViewResource(android.R.layout.simple_dropdown_item_1line);
         spinner.setAdapter(adapter);
+
+        //Add recycler viewcard
         RecyclerView recyclerView = (RecyclerView) rootView.findViewById(R.id.recyclerViewSong);
         recyclerView.setHasFixedSize(true);
         layoutManager = new LinearLayoutManager(getActivity());
@@ -56,6 +62,33 @@ public class RankFragment extends Fragment {
         songs.add(new Song("Test3","Artist",123));
         SongCardAdapter songCardAdapter = new SongCardAdapter(songs, getActivity());
         recyclerView.setAdapter(songCardAdapter);
+
+        //Swipe refresh
+        mSwipeRefreshLayout = (SwipeRefreshLayout) rootView.findViewById(R.id.rank_swipe_container);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        //First load when view created
+        mSwipeRefreshLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                mSwipeRefreshLayout.setRefreshing(true);
+                loadRecyclerViewData();
+            }
+        });
         return rootView;
+    }
+
+    @Override
+    public void onRefresh() {
+        loadRecyclerViewData();
+    }
+
+    private void loadRecyclerViewData(){
+        // Showing refresh animation before making http call
+        mSwipeRefreshLayout.setRefreshing(true);
+        Toast.makeText(getActivity(), "Ahlo", Toast.LENGTH_SHORT).show();
+
+        // Stop refresh animation
+        mSwipeRefreshLayout.setRefreshing(false);
     }
 }
