a799c21987c4ad62a56cd070d93b34103917da43
khoa <policekhoa13101998@gmail.com>
2020-05-13 08:19:21 +0700

    Add tablayout

---
+++ app/src/main/java/koha13/spasic/MainActivity.java
@@ -0,0 +1,70 @@
+package koha13.spasic;
+
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
+import androidx.viewpager.widget.ViewPager;
+
+import android.graphics.PorterDuff;
+import android.os.Build;
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        viewPager = (ViewPager) findViewById(R.id.viewpager);
+        addTabs(viewPager);
+
+        tabLayout = (TabLayout) findViewById(R.id.tabs);
+        tabLayout.setupWithViewPager(viewPager);
+        setupTabIcons();
+        tabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(viewPager){
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                super.onTabSelected(tab);
+                int tabIconColor = ContextCompat.getColor(MainActivity.this, R.color.colorPrimaryDark);
+                tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                super.onTabUnselected(tab);
+                int tabIconColor = ContextCompat.getColor(MainActivity.this, R.color.tabUnselectedIconColor);
+                tab.getIcon().setColorFilter(tabIconColor, PorterDuff.Mode.SRC_IN);
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+                super.onTabReselected(tab);
+            }
+        });
+    }
+
+    private void setupTabIcons() {
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_home_white_24dp);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_equalizer_white_24dp);
+        tabLayout.getTabAt(2).setIcon(R.drawable.ic_headset_white_24dp);
+        tabLayout.getTabAt(3).setIcon(R.drawable.ic_person_white_24dp);
+    }
+
+    private void addTabs(ViewPager viewPager) {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new AppleFragment(), "APPLE");
+        adapter.addFrag(new OrangeFragment(), "ORANGE");
+        adapter.addFrag(new OrangeFragment(), "ORANGE");
+        adapter.addFrag(new OrangeFragment(), "ORANGE");
+        viewPager.setAdapter(adapter);
+    }
+}
---
+++ app/src/main/java/koha13/spasic/ViewPagerAdapter.java
@@ -0,0 +1,35 @@
+package koha13.spasic;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentPagerAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewPagerAdapter extends FragmentPagerAdapter {
+
+    private final List mFragmentList = new ArrayList<>();
+    private final List mFragmentTitleList = new ArrayList<>();
+
+    public ViewPagerAdapter(FragmentManager manager){
+      super(manager);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        return (Fragment) mFragmentList.get(position);
+    }
+
+    @Override
+    public int getCount() {
+        return mFragmentList.size();
+    }
+
+    public void addFrag(Fragment fragment, String title) {
+        mFragmentList.add(fragment);
+        mFragmentTitleList.add(title);
+    }
+}
