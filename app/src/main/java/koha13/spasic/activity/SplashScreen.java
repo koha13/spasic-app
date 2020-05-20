e7eca009ea18e58b5ce419913eb202c5428a8ffc
khoa <policekhoa13101998@gmail.com>
2020-05-20 10:39:20 +0700

    Rev: fix layout cardview: remove paddingHoz + padding

--- app/src/main/java/koha13/spasic/activity/SplashScreen.java
+++ app/src/main/java/koha13/spasic/activity/SplashScreen.java
@@ -1,16 +1,11 @@
 package koha13.spasic.activity;
 
-import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.LinearGradient;
 import android.os.Bundle;
-import android.os.Handler;
-import android.util.Log;
 import android.view.animation.Animation;
 import android.view.animation.RotateAnimation;
 import android.widget.ImageView;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -37,13 +32,12 @@ public class SplashScreen extends AppCompatActivity {
 
         SharedPreferences prefs = getSharedPreferences("PRIVATE_DATA", MODE_PRIVATE);
         String token = prefs.getString("token", null);
-        if(token == null){
+        if (token == null) {
             Intent intent = new Intent(SplashScreen.this, LoginActivity.class);
             intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
-        }
-        else{
-            UserData.user.setId(prefs.getInt("id",0));
+        } else {
+            UserData.user.setId(prefs.getInt("id", 0));
             UserData.user.setRole(prefs.getString("role", null));
             UserData.user.setUsername(prefs.getString("username", null));
             UserData.user.setToken(prefs.getString("token", null));
