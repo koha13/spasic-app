4e48cc7cefa777bd5d3a83db8b9a8e055b5ad08a
khoa <policekhoa13101998@gmail.com>
2020-05-20 10:33:31 +0700

    Add loading screen when login + dialog when login fail + add listen to btnLogin

--- app/src/main/java/koha13/spasic/activity/LoginActivity.java
+++ app/src/main/java/koha13/spasic/activity/LoginActivity.java
@@ -1,16 +1,22 @@
 package koha13.spasic.activity;
 
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import koha13.spasic.R;
@@ -24,6 +30,10 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
     private EditText username;
     private EditText password;
     private Button lgBtn;
+    private LinearLayout loadingScreen;
+    private ImageView loadingIcon;
+    private static final float ROTATE_FROM = 0.0f;
+    private static final float ROTATE_TO = -10.0f * 360.0f;// 3.141592654f * 32.0f;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,15 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
         username = findViewById(R.id.lg_username);
         password = findViewById(R.id.lg_password);
         lgBtn = findViewById(R.id.lg_btn);
+        loadingScreen = findViewById(R.id.lg_loading_screen);
+        loadingIcon = findViewById(R.id.lg_loading_ic);
+
+        lgBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                login();
+            }
+        });
 
         password.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
@@ -48,12 +67,21 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
     private void login(){
         String us = username.getText().toString().toLowerCase().trim();
         String pw = password.getText().toString().toLowerCase().trim();
+        if(us.length()<6 || pw.length()<6){
+            onDataFail("Failed");
+        }
         UserData.login(new LoginRequest(us,pw),this);
+        loadingScreen.setVisibility(View.VISIBLE);
+        RotateAnimation r = new RotateAnimation(ROTATE_FROM, ROTATE_TO,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+        r.setDuration(3000000);
+        r.setRepeatCount(0);
+        loadingIcon.setAnimation(r);
     }
 
     @Override
     public void onDataSuccess(User data) {
-        Log.d("Login", UserData.user.getToken());
         SharedPreferences.Editor editor = (SharedPreferences.Editor) getSharedPreferences("PRIVATE_DATA",Context.MODE_PRIVATE).edit();
         editor.putString("token", UserData.user.getToken());
         editor.putString("role", UserData.user.getRole());
@@ -67,7 +95,20 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
 
     @Override
     public void onDataFail(String message) {
-        Log.d("Login", "Failed");
+        loadingScreen.setVisibility(View.INVISIBLE);
+        new AlertDialog.Builder(this)
+                .setIcon(android.R.drawable.ic_dialog_alert)
+                .setTitle("Đăng nhập không thành công")
+                .setMessage("Tài khoản hoặc mật khẩu không chính xác")
+                .setPositiveButton("OK", new DialogInterface.OnClickListener()
+                {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        username.requestFocus();
+                    }
+
+                })
+                .show();
     }
 
     @Override
--- app/src/main/java/koha13/spasic/data/UserData.java
+++ app/src/main/java/koha13/spasic/data/UserData.java
@@ -1,12 +1,8 @@
 package koha13.spasic.data;
 
-import java.io.IOException;
-import java.util.List;
-
 import koha13.spasic.api.ResponseCallback;
 import koha13.spasic.api.RetrofitClient;
 import koha13.spasic.api.SpasicApi;
-import koha13.spasic.entity.Playlist;
 import koha13.spasic.entity.User;
 import koha13.spasic.model.LoginRequest;
 import retrofit2.Call;
@@ -16,7 +12,7 @@ import retrofit2.Response;
 public class UserData {
     public static User user = new User();
 
-    public static void login(LoginRequest loginRequest, final ResponseCallback<User> callback){
+    public static void login(LoginRequest loginRequest, final ResponseCallback<User> callback) {
         SpasicApi mAPIService = RetrofitClient.getAPIService();
         mAPIService.login(loginRequest).enqueue(new Callback<User>() {
             @Override
@@ -27,11 +23,7 @@ public class UserData {
                         callback.onDataSuccess(response.body());
                 } else {
                     if (callback != null) {
-                        try {
-                            callback.onDataFail(response.errorBody().string());
-                        } catch (IOException e) {
-                            e.printStackTrace();
-                        }
+                        callback.onDataFail("failed");
                     }
                 }
             }
