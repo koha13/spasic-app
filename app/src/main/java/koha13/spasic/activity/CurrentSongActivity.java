4d776114f5096bae3d87aedae2daeba173b901bd
khoa <policekhoa13101998@gmail.com>
2020-06-03 14:45:20 +0700

    Add fragment queue list

--- app/src/main/java/koha13/spasic/activity/CurrentSongActivity.java
+++ app/src/main/java/koha13/spasic/activity/CurrentSongActivity.java
@@ -1,15 +1,16 @@
 package koha13.spasic.activity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.viewpager.widget.ViewPager;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
 
-import com.google.android.material.tabs.TabLayout;
-
+import koha13.spasic.FragmentCurrentSong.QueueFragment;
 import koha13.spasic.R;
 
 public class CurrentSongActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class CurrentSongActivity extends AppCompatActivity {
     private TextView songName;
     private TextView songArtist;
     private ImageButton queueBtn;
+    private boolean isQueue = false;
+    Fragment mFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,7 +30,7 @@ public class CurrentSongActivity extends AppCompatActivity {
         initView();
     }
 
-    private void initView(){
+    private void initView() {
         songName = findViewById(R.id.song_name);
         songName.setSelected(true);
         songArtist = findViewById(R.id.song_artist);
@@ -39,7 +42,23 @@ public class CurrentSongActivity extends AppCompatActivity {
                 finish();
             }
         });
+        mFragment = new QueueFragment();
         queueBtn = findViewById(R.id.queue_btn);
+        queueBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                if (!isQueue){
+                    fragmentManager.beginTransaction()
+                            .replace(R.id.framelayout, mFragment).commit();
+                    isQueue = true;
+                }else{
+                    fragmentManager.beginTransaction().remove(mFragment).commit();
+                    isQueue = false;
+                }
+
+            }
+        });
     }
 
 }
