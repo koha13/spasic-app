efe044ba69df37d9c9d7620fca464fd303812107
khoa <policekhoa13101998@gmail.com>
2020-06-28 22:48:30 +0700

    Add signup act

--- app/src/main/java/koha13/spasic/activity/SignupActivity.java
+++ app/src/main/java/koha13/spasic/activity/SignupActivity.java
@@ -1,5 +1,8 @@
 package koha13.spasic.activity;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.DialogInterface;
@@ -17,9 +20,6 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.Toast;
 
-import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatActivity;
-
 import java.util.List;
 
 import koha13.spasic.R;
@@ -32,13 +32,12 @@ import koha13.spasic.entity.Song;
 import koha13.spasic.entity.User;
 import koha13.spasic.model.LoginRequest;
 
-public class LoginActivity extends AppCompatActivity implements ResponseCallback<User> {
+public class SignupActivity extends AppCompatActivity implements ResponseCallback<User>{
 
     private static final float ROTATE_FROM = 0.0f;
     private static final float ROTATE_TO = -10.0f * 360.0f;// 3.141592654f * 32.0f;
     private EditText username;
     private EditText password;
-    private Button lgBtn;
     private LinearLayout loadingScreen;
     private ImageView loadingIcon;
     private Button signupBtn;
@@ -46,42 +45,34 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_signup);
 
         username = findViewById(R.id.lg_username);
         password = findViewById(R.id.lg_password);
-        lgBtn = findViewById(R.id.lg_btn);
+        signupBtn = findViewById(R.id.signup_btn);
         loadingScreen = findViewById(R.id.lg_loading_screen);
         loadingIcon = findViewById(R.id.lg_loading_ic);
         signupBtn = findViewById(R.id.signup_btn);
 
-        lgBtn.setOnClickListener(new View.OnClickListener() {
+        signupBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                login();
+                signup();
             }
         });
 
         password.setOnKeyListener(new View.OnKeyListener() {
             public boolean onKey(View v, int keyCode, KeyEvent event) {
                 if (event.getAction() == KeyEvent.ACTION_DOWN && keyCode == KeyEvent.KEYCODE_ENTER) {
-                    login();
+                    signup();
                     return true;
                 }
                 return false;
             }
         });
-
-        signupBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(LoginActivity.this, SignupActivity.class);
-                startActivity(intent);
-            }
-        });
     }
 
-    private void login() {
+    private void signup() {
         hideKeyboard(this);
         String us = username.getText().toString().toLowerCase().trim();
         String pw = password.getText().toString().toLowerCase().trim();
@@ -148,7 +139,7 @@ public class LoginActivity extends AppCompatActivity implements ResponseCallback
                 AllPlaylistsViewModel.fetchAllPlaylists(new ResponseCallback<List<Playlist>>() {
                     @Override
                     public void onDataSuccess(List<Playlist> data) {
-                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        Intent intent = new Intent(SignupActivity.this, MainActivity.class);
                         intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
                         startActivity(intent);
                     }
